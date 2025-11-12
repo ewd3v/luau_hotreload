20acc111643fb0416b64e773feb68e3c791276e3
ewd3v <74792428+ewd3v@users.noreply.github.com>
2025-11-12 13:37:26 +0100

    fix fiu not being a part of build files on roblox target

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Fiu is now included as a build file on Roblox targets (should fix issue with it not syncing in)
+
 ## [0.1.1] - 2025-11-06
 
 ### Changed
--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,7 @@ pesde x ewdev/multitarget -- build --yes \
     --build-files src --output dist \
     --global RUNTIME --require-mode luau \
     --luau --roblox # Runtimes to build for
+
+sed -i -e \
+    "s/build_files = \[\"src\"\]/build_files = \[\"src\", \"fiu\/Source.lua\"\]/" \
+    dist/roblox/pesde.toml
